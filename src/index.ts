d97c86bfc8d81fc18a3935f768a3480833217661
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-17 01:31:05 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import routeBook from "./routes/book";
 import routeUpload from "./routes/uploadFile";
+import routeProtected from "./routes/protectedRoute";
 
 dotenv.config();
+
 
 const app = express();
 app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 app.use('/api', routeBook, routeUpload)
+app.use('/api', routeProtected)
 const port = process.env.PORT || 8082;
-
-// app.get("/libros", (_req:Request, res:Response) => {
-//     res.send("Bienvenido a la app de libros");
-//   });
 
 app.listen(port, () => {
   console.log(`BOOKS_API: Listening on port ${port}`);
---
+++ src/routes/protectedRoute.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { verifyTokenMW } from '../middleware/verifyTokenMW';
+
+const routeProtected = express.Router();
+
+routeProtected.use(verifyTokenMW);
+
+routeProtected.get('/book/login', (req, res) => {
+  res.json({ message: 'Esta es una ruta protegida.' });
+});
+
+export default routeProtected;
